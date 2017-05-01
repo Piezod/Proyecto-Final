f64ec7d4d8bba62bca9d5525e79d56ffa53aab7e
Piezod <marte69@gmail.com>
2017-05-01 23:32:01 +0200

    admin001

---
+++ src/serverlets/ServeletZonaAdmin.java
@@ -0,0 +1,73 @@
+package serverlets;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Utilidades.SolicitudesAdmin;
+
+/**
+ * Servlet implementation class ServeletZonaAdmin
+ */
+@WebServlet("/ServeletZonaAdmin")
+public class ServeletZonaAdmin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServeletZonaAdmin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String opcion=(String)request.getParameter("opcion");
+		System.out.println("opcion del menu de administracion "+opcion);
+		switch (opcion) {
+		case "solicitud":
+			
+			SolicitudesAdmin sa=new SolicitudesAdmin();
+			
+			try {
+				HashMap<String, ArrayList> hml2=sa.Solicitudespendientes();
+				request.setAttribute("solicitudpendiente", hml2);
+				request.setAttribute("usuario", request.getParameter("usuario"));
+				String nextJSP = "/Solicitudes";
+				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
+				dispatcher.forward(request,response);
+				
+			} catch (ClassNotFoundException | SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			break;
+
+		default:
+			break;
+		}
+	}
+
+}
